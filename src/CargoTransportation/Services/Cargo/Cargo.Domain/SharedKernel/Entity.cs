04c709480336ac2b0b779e8d49fdbaedcea348c3
Anderweys <anderweys999@gmail.com>
2023-02-07 22:53:54 +0300

    Add ShaderKernel: Entity, ValueObject, IAggregateRoot, IRepository, IUnitOfWork.

---
+++ src/CargoTransportation/Services/Cargo/Cargo.Domain/SharedKernel/Entity.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+
+namespace Cargo.Domain.SeedWork;
+
+
+/// <summary>
+/// The base entity.
+/// </summary>
+public abstract class Entity
+{
+    /// <summary>
+    /// The identifier.
+    /// Is a important entity's member.
+    /// </summary>
+    private int _id;
+
+    public virtual int Id
+    {
+        get => Id;
+        protected set
+        {
+            _id = value;
+        }
+    }
+
+    /// <summary>
+    /// The domain events.
+    /// Using for saving domain events from other aggregates.
+    /// </summary>
+    private List<INotification> _domainEvents;
+    public IReadOnlyCollection<INotification> DomainEvents => _domainEvents.AsReadOnly();
+
+    public void AddDomainEvent(INotification domainEvent)
+    {
+        _domainEvents ??= new List<INotification>();
+        _domainEvents.Add(domainEvent);
+    }
+    public void RemoveDomainEvent(INotification domainEvent)
+    {
+        _domainEvents.Remove(domainEvent);
+    }
+    public void ClearDomainEvent()
+    {
+        _domainEvents.Clear();
+    }
+}
---
+++ src/CargoTransportation/Services/Cargo/Cargo.Domain/SharedKernel/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace Cargo.Domain.SeedWork;
+
+
+public interface IUnitOfWork : IDisposable
+{
+    Task SaveChangesAsync(CancellationToken token = default(CancellationToken));
+    Task SaveEntitiesAsync(CancellationToken token = default(CancellationToken));
+}
