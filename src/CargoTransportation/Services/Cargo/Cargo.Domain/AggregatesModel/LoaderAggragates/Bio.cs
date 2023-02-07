1c4b243ac877e4896d925f3d6b0b7f5df6211f93
Anderweys <anderweys999@gmail.com>
2023-02-07 23:56:00 +0300

    Add Loader AggregateRoot.

---
+++ src/CargoTransportation/Services/Cargo/Cargo.Domain/AggregatesModel/LoaderAggragates/Bio.cs
@@ -0,0 +1,24 @@
+﻿using Cargo.Domain.SeedWork;
+
+namespace Cargo.Domain.AggregatesModel.LoaderAggragates;
+
+
+public class Bio : ValueObject
+{
+    public string LastName { get; private set; }
+    public string FirstName { get; private set; }
+    public string MiddleName { get; private set; }
+    public short Age { get; private set; }
+
+    public Bio()
+    {
+    }
+
+    public Bio(string lastName, string firstName, string middleName, short age)
+    {
+        LastName=lastName;
+        FirstName=firstName;
+        MiddleName=middleName;
+        Age=age;
+    }
+}
