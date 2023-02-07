ce5c401e1fef1b4ffb774638b6642813f03f46f5
Anderweys <anderweys999@gmail.com>
2023-02-07 23:33:56 +0300

    Add DomainEvent CargoPlaced.

---
+++ src/CargoTransportation/Services/Cargo/Cargo.Domain/Events/CargoPlacedDomainEvent.cs
@@ -0,0 +1,24 @@
+﻿using CargoObject = Cargo.Domain.AggregatesModel.CargoAggregates.Cargo;
+using MediatR;
+
+namespace Cargo.Domain.Events;
+
+/// <summary>
+/// It's when a cargo prepared for loading.
+/// </summary>
+public class CargoPlacedDomainEvent : INotification
+{
+    public int LoaderId { get; }
+    public string Description { get; }
+    public CargoObject Cargo { get; }
+    public DateTime LoadData { get; }
+
+    public CargoPlacedDomainEvent(int loaderId, string description, CargoObject cargo, DateTime loadData)
+    {
+        LoaderId = loaderId;
+        Description=description;
+        Cargo=cargo;
+        LoadData=loadData;
+    }
+}
+
