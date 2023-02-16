e1b45a0b1352e97f32d7d6c8a9bcf95b5aeddd01
Anderweys <anderweys999@gmail.com>
2023-02-16 23:53:52 +0300

    Add Controller in WebMVC.

---
+++ src/CargoTransportation/Services/Web/WebMVC/Controllers/CargoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers;
+
+public class CargoController : Controller
+{
+    public IActionResult Index() => View();
+
+    // TODO.
+    //public string Index(User user, IEnumerable<Item> items)
+    //{
+    //    return Ok();
+    //}
+}
