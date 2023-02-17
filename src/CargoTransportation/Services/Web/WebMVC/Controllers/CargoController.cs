f52827f689b41119302fc053339894ad8dbfd79b
Anderweys <anderweys999@gmail.com>
2023-02-17 23:55:47 +0300

    Change Cargo controller, view model and razor page for local test.

--- src/CargoTransportation/Services/Web/WebMVC/Controllers/CargoController.cs
+++ src/CargoTransportation/Services/Web/WebMVC/Controllers/CargoController.cs
@@ -5,11 +5,23 @@ namespace WebMVC.Controllers;
 
 public class CargoController : Controller
 {
+
+    private readonly ILogger<CargoController> _logger;
+
+    public CargoController(ILogger<CargoController> logger)
+    {
+        _logger=logger;
+    }
+
     public IActionResult Index() => View();
 
-    // TODO.
-    //public string Index(User user, IEnumerable<Item> items)
-    //{
-    //    return Ok();
-    //}
+    [HttpPost]
+    public IActionResult Index([FromForm] User user)
+    {
+        _logger.LogInformation($"Query in CargoController: {user.Name} {user.Email}");
+
+        // TODO: Get result from Cargo service.
+
+        return Ok("cargo");
+    }
 }
