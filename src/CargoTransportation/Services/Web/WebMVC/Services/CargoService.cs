bdf72e27230a17a064365eea439973f9d5bd7f68
Anderweys <anderweys999@gmail.com>
2023-02-18 21:56:21 +0300

    Add Cargo Service.

---
+++ src/CargoTransportation/Services/Web/WebMVC/Services/CargoService.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using WebMVC.ViewModels;
+
+namespace WebMVC.Services;
+
+public class CargoService : ICargoService
+{
+    private readonly HttpClient _httpClient;
+    private readonly ILogger<CargoService> _logger;
+
+    public CargoService(HttpClient httpClient, ILogger<CargoService> logger)
+    {
+        _httpClient=httpClient;
+        _logger=logger;
+    }
+
+    public async Task<Cargo> GetCargo()
+    {
+        var uri = "http://localhost:5250/api/v1/cargo";
+        var response = await _httpClient.GetStringAsync(uri);
+
+        var cargo = JsonSerializer.Deserialize<Cargo>(response, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+        _logger.LogInformation($"CargoService here: {cargo.Name} {cargo.Email}");
+
+        return cargo;
+    }
+}
