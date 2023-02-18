7ad8b6a862c928ec98d03c4d994909753f2a7654
Anderweys <anderweys999@gmail.com>
2023-02-18 21:57:24 +0300

    Change CargoController: Now http-request to real CargoService.

--- src/CargoTransportation/Services/Web/WebMVC/Controllers/CargoController.cs
+++ src/CargoTransportation/Services/Web/WebMVC/Controllers/CargoController.cs
@@ -1,27 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebMVC.Models;
+using WebMVC.Services;
 
 namespace WebMVC.Controllers;
 
 public class CargoController : Controller
 {
-
     private readonly ILogger<CargoController> _logger;
+    private ICargoService _cargoService; 
 
-    public CargoController(ILogger<CargoController> logger)
+    public CargoController(ILogger<CargoController> logger, ICargoService cargoService)
     {
         _logger=logger;
+        _cargoService=cargoService;
     }
 
     public IActionResult Index() => View();
 
     [HttpPost]
-    public IActionResult Index([FromForm] User user)
+    public async Task<IActionResult> Index([FromForm] User user)
     {
         _logger.LogInformation($"Query in CargoController: {user.Name} {user.Email}");
 
+        var cargo = await _cargoService.GetCargo();
         // TODO: Get result from Cargo service.
-
-        return Ok("cargo");
+        _logger.LogInformation($"Gotcha cargo!: {cargo.Name} {cargo.Email}");
+        return Ok(cargo);
     }
 }
