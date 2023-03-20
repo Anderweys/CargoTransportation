5b2c4ec155ec57f22651b902d8afa773ba2606f2
Anderweys <anderweys999@gmail.com>
2023-03-20 20:08:53 +0300

    Add Cargo CQRS Command: AddItemInMemory.

---
+++ src/CargoTransportation/Services/Cargo/Cargo.Application/Commands/Command/AddItemsInMemoryCacheCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using CargoObject.Application.Models;
+using System.Runtime.Serialization;
+
+namespace CargoObject.Application.Commands.Command;
+
+[DataContract]
+public class AddItemsInMemoryCacheCommand : IRequest<bool>
+{
+    [DataMember]
+    public string UserId { get; init; }
+    [DataMember]
+    public IEnumerable<Item> Items { get; init; }
+
+    public AddItemsInMemoryCacheCommand(string userId, IEnumerable<Item> items)
+    {
+        UserId = userId;
+        Items = items;
+    }
+}
