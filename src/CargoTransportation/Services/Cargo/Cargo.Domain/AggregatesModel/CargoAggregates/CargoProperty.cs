64d5e19111886a34fd7ef6c361a04cd9f0f332d3
Anderweys <anderweys999@gmail.com>
2023-03-20 20:39:05 +0300

    Change Cargo.Domain: change CargoProperties logic.

--- src/CargoTransportation/Services/Cargo/Cargo.Domain/AggregatesModel/CargoAggregates/CargoProperty.cs
+++ src/CargoTransportation/Services/Cargo/Cargo.Domain/AggregatesModel/CargoAggregates/CargoProperty.cs
@@ -2,36 +2,23 @@
 
 namespace CargoObject.Domain.AggregatesModel.CargoAggregates;
 
-
 public class CargoProperty : ValueObject
 {
-    public LimitValue Temperature { get; private set; }
-    public LimitValue Pressure { get; private set; }
+    public float MaxTemperature { get; private set; }
+    public float MinTemperature { get; private set; }
+    public float MaxPressure { get; private set; }
+    public float MinPressure { get; private set; }
 
     public CargoProperty()
     {
     }
 
     public CargoProperty(float maxTemperature, float minTemperature,
-        float minPressure, float maxPressure)
+        float maxPressure, float minPressure)
     {
-        Temperature = new() { Max = maxTemperature, Min = minTemperature };
-        Pressure = new() { Max = maxPressure, Min = minPressure };
-    }
-}
-
-
-/// <summary>
-/// Using for min and max value for different cargo's properties.
-/// </summary>
-public struct LimitValue
-{
-    public float Min { get; set; }
-    public float Max { get; set; }
-
-    public LimitValue()
-    {
-        Min = 0;
-        Max = 0;
+        MaxTemperature=maxTemperature;
+        MinTemperature=minTemperature;
+        MaxPressure=maxPressure;
+        MinPressure=minPressure;
     }
 }
