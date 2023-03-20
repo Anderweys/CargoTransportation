549ad24397292c29854be61b12f7377a382e0509
Anderweys <anderweys999@gmail.com>
2023-03-20 21:15:37 +0300

    Change HttpClient extension in WebMVC: add HttpClient to services.

--- src/CargoTransportation/Services/Web/WebMVC/Extensions/HttpClientServiceExtension.cs
+++ src/CargoTransportation/Services/Web/WebMVC/Extensions/HttpClientServiceExtension.cs
@@ -7,6 +7,8 @@ public static class HttpClientServiceExtension
     public static IServiceCollection AddHttpClientServices(this IServiceCollection services)
     {
         services.AddHttpClient<ICargoService, CargoService>();
+        services.AddHttpClient<ITransportService, TransportService>();
+        services.AddHttpClient<IRoutingService, RoutingService>();
 
         return services;
     }
