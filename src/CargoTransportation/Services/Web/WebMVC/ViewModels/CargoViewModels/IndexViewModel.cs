efcde11f46f3333a4e8ae1675e3dfa1b88a8df1c
Anderweys <anderweys999@gmail.com>
2023-03-20 21:25:25 +0300

    Change Cargo IndexViewModel in WebMVC.

--- src/CargoTransportation/Services/Web/WebMVC/ViewModels/CargoViewModels/IndexViewModel.cs
+++ src/CargoTransportation/Services/Web/WebMVC/ViewModels/CargoViewModels/IndexViewModel.cs
@@ -4,6 +4,8 @@ namespace WebMVC.ViewModels.CargoViewModels;
 
 public class IndexViewModel
 {
-    public User User { get; set; }
-    public IEnumerable<Item> Items { get; set; } = new List<Item>();   
+    public string City { get; set; }
+    public IEnumerable<ItemModel> Items { get; set; } = new List<ItemModel>();
+    public DateTime Time { get; set; }
+    public float Price { get; set; }
 }
