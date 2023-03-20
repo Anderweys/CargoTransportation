93fcb8d7b482ecd3c6d4000e2142e1fe020b6b0c
Anderweys <anderweys999@gmail.com>
2023-03-20 21:04:57 +0300

    Add CalculationPrice Service in Transport.

---
+++ src/CargoTransportation/Services/Transportation/Transport.API/Transport.API/Services/CalculationPriceService.cs
@@ -0,0 +1,30 @@
+﻿using Transportation.API.Models.DTOs;
+
+namespace Transportation.API.Services;
+
+public class CalculationPriceService
+{
+    private enum TransportTypeFactor : int
+    {
+        GROUND = 1,
+        WATER = 2,
+        AIR = 5
+    }
+    private const float CUSTOM_PERCENT = 0.25f;
+
+    public float CalculatePrice(string type, IEnumerable<ItemDTO> itemsDTO)
+    {
+        var coefficient = type switch
+        {
+            "Ground" => TransportTypeFactor.GROUND,
+            "Water" => TransportTypeFactor.WATER,
+            "Air" => TransportTypeFactor.AIR,
+            _ => throw new NotImplementedException("No transport type. Error in transport service!")
+        };
+
+        float coef = (float)coefficient;
+        var totalPrice = itemsDTO.Sum(i => i.Price) * CUSTOM_PERCENT * coef;
+
+        return totalPrice;
+    }
+}
