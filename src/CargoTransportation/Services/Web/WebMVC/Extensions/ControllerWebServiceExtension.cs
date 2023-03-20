7bbc91c8dc713332df48be4067eab448ebfb9da0
Anderweys <anderweys999@gmail.com>
2023-03-20 21:14:41 +0300

    Add WebServices extension for WebMVC controller services.

---
+++ src/CargoTransportation/Services/Web/WebMVC/Extensions/ControllerWebServiceExtension.cs
@@ -0,0 +1,13 @@
+﻿using WebMVC.Services;
+
+namespace WebMVC.Extensions;
+
+public static class ControllerWebServiceExtension
+{
+    public static void AddControllerWebServices(this IServiceCollection services)
+    {
+        services.AddSingleton<ICargoService, CargoService>();
+        services.AddSingleton<ITransportService, TransportService>();
+        services.AddSingleton<IRoutingService, RoutingService>();
+    }
+}
