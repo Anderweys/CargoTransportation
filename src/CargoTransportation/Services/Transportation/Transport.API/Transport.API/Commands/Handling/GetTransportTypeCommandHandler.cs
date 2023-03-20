b890d00b32a2939edeb1729ae3616e34713568e0
Anderweys <anderweys999@gmail.com>
2023-03-20 21:00:16 +0300

    Add CQRS handler in Transport: GetTransportType.

---
+++ src/CargoTransportation/Services/Transportation/Transport.API/Transport.API/Commands/Handling/GetTransportTypeCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Transportation.API.Commands.Command;
+using Transportation.API.Models;
+using Transportation.API.Models.DTOs;
+
+namespace Transportation.API.Commands.Handling;
+
+public class GetTransportTypeCommandHandler : IRequestHandler<GetTransportTypeCommand, IEnumerable<TransportTypeDTO>>
+{
+    private readonly ITransportRepository _transportRepository;
+
+    public GetTransportTypeCommandHandler(ITransportRepository transportRepository)
+    {
+        _transportRepository = transportRepository ?? throw new ArgumentNullException(nameof(transportRepository));
+    }
+
+    public async Task<IEnumerable<TransportTypeDTO>> Handle(GetTransportTypeCommand request, CancellationToken cancellationToken)
+    {
+        var transports = await _transportRepository.GetAllAsync();
+
+        List<TransportTypeDTO> result = new();
+
+        foreach (var item in transports)
+        {
+            result.Add(new(item.Name, item.Type));
+        }
+
+        return result;
+    }
+}
