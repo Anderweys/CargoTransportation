4f88091054905fe65f7b6612326da4ed67e42809
Anderweys <anderweys999@gmail.com>
2023-03-20 20:55:46 +0300

    Change program in Routing: add services and config.

--- src/CargoTransportation/Services/Routing/Routing.API/Program.cs
+++ src/CargoTransportation/Services/Routing/Routing.API/Program.cs
@@ -1,6 +1,29 @@
+using Routing.API.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    options.InstanceName = "Redis";
+});
+builder.Services.AddMongo(builder.Configuration.GetSection("Mongo"));
+builder.Services.AddMassTransitRabbitMQ(builder.Configuration.GetSection("RabbitMQ"));
+builder.Services.AddCityRepository();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapControllers();
 
 app.Run();
+
