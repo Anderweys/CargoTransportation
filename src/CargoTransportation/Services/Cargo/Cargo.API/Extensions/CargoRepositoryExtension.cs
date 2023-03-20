0da3e70defa7346181f0181fdfff4f7025fd1016
Anderweys <anderweys999@gmail.com>
2023-03-20 20:03:04 +0300

    Add Repository Extension: CargoRepository.

---
+++ src/CargoTransportation/Services/Cargo/Cargo.API/Extensions/CargoRepositoryExtension.cs
@@ -0,0 +1,12 @@
+﻿using CargoObject.Domain.AggregatesModel.CargoAggregates;
+using CargoObject.Infrastructure.Repositories;
+
+namespace CargoObject.API.Extensions;
+
+public static class CargoRepositoryExtension
+{
+    public static void AddCargoRepository(this IServiceCollection services)
+    {
+        services.AddScoped<ICargoRepository, CargoRepository>();
+    }
+}
