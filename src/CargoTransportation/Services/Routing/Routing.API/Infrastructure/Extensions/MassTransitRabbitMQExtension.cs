9e012b8332589ec692362579b13149d7b1c965c3
Anderweys <anderweys999@gmail.com>
2023-03-20 20:49:31 +0300

    Add MassTransitExtension in Routing: configure RabbitMQ as Event Bus.

---
+++ src/CargoTransportation/Services/Routing/Routing.API/Infrastructure/Extensions/MassTransitRabbitMQExtension.cs
@@ -0,0 +1,30 @@
+﻿using MassTransit;
+using Contracts.IntegrationEvents;
+
+namespace Routing.API.Infrastructure.Extensions;
+
+public static class MassTransitRabbitMQExtension
+{
+    public static void AddMassTransitRabbitMQ(this IServiceCollection services, IConfiguration congif)
+    {
+        services.AddMassTransit(c =>
+        {
+            c.UsingRabbitMq((ctx, cfg) =>
+            {
+                cfg.Host(congif.GetSection("ConnectionString").Value, host =>
+                {
+                    host.Username(congif.GetSection("Username").Value);
+                    host.Password(congif.GetSection("Password").Value);
+                });
+
+                var ep = congif.GetSection("MicroservicesEndpoints").GetSection("Routing");
+
+                cfg.Message<CreateCargoIntegrationEvent>(m =>
+                {
+                    m.SetEntityName(ep.GetSection("Publisher").GetSection("Name").Value);
+                });
+            });
+        });
+    }
+}
+
