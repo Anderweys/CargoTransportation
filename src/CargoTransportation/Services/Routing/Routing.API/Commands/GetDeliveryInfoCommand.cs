54e4456f4aa9d7e44e143a2c32478b2a9e2dece9
Anderweys <anderweys999@gmail.com>
2023-03-20 20:47:03 +0300

    Add CQRS command in Routing.API: GetDeliveryInfo.

---
+++ src/CargoTransportation/Services/Routing/Routing.API/Commands/GetDeliveryInfoCommand.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Routing.API.Models.DTOs;
+using System.Runtime.Serialization;
+
+namespace Routing.API.Commands;
+
+[DataContract]
+public class GetDeliveryInfoCommand : IRequest<DeliveryInfoDTO>
+{
+    [DataMember]
+    public string City { get; init; }
+
+    [DataMember]
+    public string UserId { get; init; }
+
+    public GetDeliveryInfoCommand()
+    {
+    }
+
+    public GetDeliveryInfoCommand(string city, string userId)
+    {
+        City = city;
+        UserId = userId;
+    }
+}
