21d7015d9df05e127b61f06290cf2683be541985
Anderweys <anderweys999@gmail.com>
2023-03-20 20:43:01 +0300

    Add CargoProperty EntityConfiguration for EF Core.

---
+++ src/CargoTransportation/Services/Cargo/Cargo.Infrastructure/EntityInitConfig/CargoPropertyEntityConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using CargoObject.Domain.AggregatesModel.CargoAggregates;
+
+namespace CargoObject.Infrastructure.EntityInitConfig;
+
+public class CargoPropertyEntityConfig : IEntityTypeConfiguration<CargoProperty>
+{
+    public void Configure(EntityTypeBuilder<CargoProperty> builder)
+    {
+        builder.ToTable(nameof(CargoProperty));
+
+        builder.Property<int>("Id")
+            .IsRequired();
+    }
+}
