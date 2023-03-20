0463128379083ba6960323573d0789e07a5a6c8b
Anderweys <anderweys999@gmail.com>
2023-03-20 21:02:20 +0300

    Add TransportRepository in Transport.

---
+++ src/CargoTransportation/Services/Transportation/Transport.API/Transport.API/Infrastructure/Extensions/TransportRepositoryExtension.cs
@@ -0,0 +1,12 @@
+﻿using Transportation.API.Models;
+using Transportation.API.Infrastructure.Repositories;
+
+namespace Transportation.API.Infrastructure.Extensions;
+
+public static class TransportRepositoryExtension
+{
+    public static void AddTransportRepository(this IServiceCollection services)
+    {
+        services.AddSingleton<ITransportRepository, TransportRepository>();
+    }
+}
