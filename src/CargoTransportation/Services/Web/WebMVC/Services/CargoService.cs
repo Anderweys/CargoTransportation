2ee4ea20e532f39baaf62444a6ba6df71f0e83ae
Anderweys <anderweys999@gmail.com>
2023-03-20 21:24:13 +0300

    Add CargoService in WebMVC.

--- src/CargoTransportation/Services/Web/WebMVC/Services/CargoService.cs
+++ src/CargoTransportation/Services/Web/WebMVC/Services/CargoService.cs
@@ -1,5 +1,7 @@
-﻿using System.Text.Json;
-using WebMVC.ViewModels;
+﻿using System.Net;
+using System.Text.Json;
+using WebMVC.Models.DTOs;
+using WebMVC.Infrastructure.API;
 
 namespace WebMVC.Services;
 
@@ -8,23 +10,68 @@ public class CargoService : ICargoService
     private readonly HttpClient _httpClient;
     private readonly ILogger<CargoService> _logger;
 
-    public CargoService(HttpClient httpClient, ILogger<CargoService> logger)
+    private readonly string _remoteServiceUrl;
+
+    public CargoService(HttpClient httpClient, ILogger<CargoService> logger, IConfiguration configuration)
     {
-        _httpClient=httpClient;
-        _logger=logger;
+        _httpClient=httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        _logger=logger ?? throw new ArgumentNullException(nameof(logger));
+
+        // Microservices -> Cargo -> url -> smth_url.
+        _remoteServiceUrl=configuration
+            .GetSection("Microservices")
+            .GetSection("Cargo")
+            .GetSection("url").Value ?? throw new ArgumentNullException(nameof(configuration));
     }
 
-    public async Task<Cargo> GetCargo()
+    public async Task AddItemsAsync(UserItemsDTO userItems)
     {
-        var uri = "http://localhost:5250/api/v1/cargo";
-        var response = await _httpClient.GetStringAsync(uri);
+        var uri = API.Cargo.AddItems(_remoteServiceUrl);
+        var userItemsContent = new StringContent(JsonSerializer.Serialize(userItems), System.Text.Encoding.UTF8, "application/json");
+        var response = await _httpClient.PostAsync(uri, userItemsContent);
 
-        var cargo = JsonSerializer.Deserialize<Cargo>(response, new JsonSerializerOptions
+        if (response.StatusCode == HttpStatusCode.BadRequest)
         {
-            PropertyNameCaseInsensitive = true
-        });
-        _logger.LogInformation($"CargoService here: {cargo.Name} {cargo.Email}");
+            _logger.LogCritical(
+                "Error adding items. Service:{service} Time: {Time}.",
+                nameof(CargoService),
+                DateTime.UtcNow.ToLongDateString());
+        }
+        else
+        {
+            response.EnsureSuccessStatusCode();
+        }
+    }
 
-        return cargo;
+    public async Task<IEnumerable<CargoInfoDTO>?> GetCargoInfoAsync(string userId)
+    {
+        IEnumerable<CargoInfoDTO>? cargoInfoDto = null;
+        var uri = API.Cargo.GetCargo(_remoteServiceUrl, userId);
+
+        try
+        {
+            var response = await _httpClient.GetStringAsync(uri);
+
+            cargoInfoDto = JsonSerializer.Deserialize<IEnumerable<CargoInfoDTO>>(response, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        }
+        catch (HttpRequestException)
+        {
+            _logger.LogError(
+                "Cargo mircoservice return error. Service: {service}, Time: {time}.",
+                nameof(CargoService),
+                DateTime.UtcNow.ToLongDateString());
+        }
+        catch (JsonException)
+        {
+            _logger.LogCritical(
+                "Cannot read json-response. Service: {service}, Time: {time}.",
+                nameof(CargoService),
+                DateTime.UtcNow.ToLongDateString());
+        }
+
+        return cargoInfoDto;
     }
 }
