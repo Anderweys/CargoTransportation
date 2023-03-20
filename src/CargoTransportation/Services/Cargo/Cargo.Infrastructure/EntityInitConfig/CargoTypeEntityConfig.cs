4a56856ec756dbd0812a9d129f2c0706dbcbd728
Anderweys <anderweys999@gmail.com>
2023-03-20 20:43:18 +0300

    Add CargoType EntityConfiguration for EF Core.

---
+++ src/CargoTransportation/Services/Cargo/Cargo.Infrastructure/EntityInitConfig/CargoTypeEntityConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using CargoObject.Domain.AggregatesModel.CargoAggregates;
+
+namespace CargoObject.Infrastructure.EntityInitConfig;
+
+public class CargoTypeEntityConfig : IEntityTypeConfiguration<CargoType>
+{
+    public void Configure(EntityTypeBuilder<CargoType> builder)
+    {
+        builder.ToTable(nameof(CargoType));
+        builder.HasKey(t => t.Id);
+
+        builder.Ignore(t => t.DomainEvents);
+    }
+}
