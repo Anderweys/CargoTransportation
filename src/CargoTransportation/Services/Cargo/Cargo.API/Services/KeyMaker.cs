8086746dd0806d4506d9e24b409e02b0f299f95a
Anderweys <anderweys999@gmail.com>
2023-03-20 20:06:08 +0300

    Add keymaker service: use for create keys to Mongo.

---
+++ src/CargoTransportation/Services/Cargo/Cargo.API/Services/KeyMaker.cs
@@ -0,0 +1,23 @@
+﻿namespace CargoObject.API.Services;
+
+/// <summary>
+/// Service for generation keys that using for set value in distributed cache (like Redis etc.)
+/// Example: 
+///     name, user, option.
+/// Result:
+///     name:user:option:
+/// </summary>
+public static class KeyMaker
+{
+    public static string CreateKey(params string[] keys)
+    {
+        var result = string.Empty;
+
+        foreach (var key in keys)
+        {
+            result += key + ":";
+        }
+
+        return result;
+    }
+}
