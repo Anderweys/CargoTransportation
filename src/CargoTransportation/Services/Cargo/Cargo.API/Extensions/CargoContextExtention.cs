7956d87305f8941a0e63e2981980c2f532048d1a
Anderweys <anderweys999@gmail.com>
2023-06-23 18:29:55 +0300

    Rename extension: AddCargoContextExtension -> CargoContextExtension.

--- src/CargoTransportation/Services/Cargo/Cargo.API/Extensions/CargoContextExtention.cs
+++ src/CargoTransportation/Services/Cargo/Cargo.API/Extensions/CargoContextExtention.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CargoObject.API.Extensions;
 
-public static class AddCargoContextExtention
+public static class CargoContextExtention
 {
     public static void AddCargoContext(this IServiceCollection services, IConfiguration configuration)
         => services.AddDbContext<CargoContext>(options
