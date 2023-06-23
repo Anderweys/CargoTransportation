b9e2b49d6c2c234a9150215d3f0c58bf4e8ecf05
Anderweys <anderweys999@gmail.com>
2023-06-23 18:28:29 +0300

    Add infrastructure layer: DbContext and Repository.

---
+++ src/CargoTransportation/Services/Account/Account.API/Infrastructure/AccountRepository.cs
@@ -0,0 +1,36 @@
+﻿using Account.API.Domain.Entities;
+using Account.API.Domain.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace Account.API.Infrastructure;
+
+public class AccountRepository : IAccountRepository
+{
+    private readonly AccountContext _context;
+
+    public AccountRepository(AccountContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task<bool> AddAsync(User user)
+    {
+        var isExist = await _context.Users
+            .FirstOrDefaultAsync(u => u.Login == user.Login && u.Password == user.Password);
+        if (isExist is null)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public async Task<User?> GetAsync(User user)
+         => await _context.Users
+            .FirstOrDefaultAsync(u => u.Login == user.Login && u.Password == user.Password);
+}
