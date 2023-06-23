114b2f9298014870345b882137bf2e0fb5f55e35
Anderweys <anderweys999@gmail.com>
2023-06-23 18:26:49 +0300

    Add CQRS.

---
+++ src/CargoTransportation/Services/Account/Account.API/Application/Queries/GetUserAccountQuery.cs
@@ -0,0 +1,24 @@
+﻿using Account.API.Domain.Entities;
+using MediatR;
+using System.Runtime.Serialization;
+
+namespace Account.API.Application.Queries;
+
+[DataContract]
+public class GetUserAccountQuery : IRequest<User?>
+{
+    [DataMember]
+    public string Login { get; set; }
+    [DataMember]
+    public string Password { get; set; }
+
+    public GetUserAccountQuery()
+    {
+    }
+
+    public GetUserAccountQuery(string login, string password)
+    {
+        Login=login;
+        Password=password;
+    }
+}
