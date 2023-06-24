9901b90bedd108e73f245181ec051861b5cf11c0
Anderweys <anderweys999@gmail.com>
2023-06-24 11:12:29 +0300

    Relocate to Application layer.

--- src/CargoTransportation/Services/Transportation/Transport.API/Transport.API/Commands/Command/GetTransportTypeCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using System.Runtime.Serialization;
-using Transportation.API.Models.DTOs;
-
-namespace Transportation.API.Commands.Command;
-
-[DataContract]
-public class GetTransportTypeCommand : IRequest<IEnumerable<TransportTypeDTO>>
-{ }
