e039b515f69bc40c7c569cba612286f497c9ff26
Anderweys <anderweys999@gmail.com>
2023-06-30 21:32:22 +0300

    Rename ViewModels.

--- src/CargoTransportation/Services/Web/WebMVC/ViewModels/RoutingViewModels/IndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebMVC.ViewModels.RoutingViewModels;
-
-public class IndexViewModel
-{
-    public string City { get; set; }
-    public DateTime Time { get; set; }
-    public IEnumerable<ItemModel> Items { get; set; } = Enumerable.Empty<ItemModel>();
-    public float Price { get; set; }
-}
