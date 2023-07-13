4cc00a74942eaec43150b8e896d610df5339bb78
Anderweys <anderweys999@gmail.com>
2023-07-13 21:46:31 +0300

    Change view model for item list.

--- src/CargoTransportation/Web/WebMVC/ViewModels/Routing/RoutingViewModel.cs
+++ src/CargoTransportation/Web/WebMVC/ViewModels/Routing/RoutingViewModel.cs
@@ -1,9 +1,7 @@
 ﻿namespace WebMVC.ViewModels.RoutingViewModels;
 
-public class RoutingViewModel
-{
-    public string City { get; set; }
-    public DateTime Time { get; set; }
-    public IEnumerable<ItemModel> Items { get; set; } = Enumerable.Empty<ItemModel>();
-    public float Price { get; set; }
-}
+public record RoutingViewModel(
+    string City,
+    DateTime Time,
+    IEnumerable<ItemModel> Items,
+    float Price);
