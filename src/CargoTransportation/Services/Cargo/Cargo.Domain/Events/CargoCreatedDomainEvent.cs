8f9cb6ee925db2d08534b723f5c927d41eb85c39
Anderweys <anderweys999@gmail.com>
2023-08-02 19:21:37 +0300

    Add Events for Event Sorcing.

---
+++ src/CargoTransportation/Services/Cargo/Cargo.Domain/Events/CargoCreatedDomainEvent.cs
@@ -0,0 +1,14 @@
+﻿using CargoObject.Domain.AggregatesModel.CargoAggregates;
+
+namespace CargoObject.Domain.Events;
+
+public record CargoCreatedDomainEvent(
+    Guid Id,
+    string Name,
+    string City,
+    float Price,
+    DateTime Time,
+    float CurrentVolume,
+    CargoType CargoType,
+    List<CargoItem> CargoItems
+);
