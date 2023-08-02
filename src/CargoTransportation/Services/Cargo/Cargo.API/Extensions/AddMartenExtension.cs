08298e3d36756d35c1e1e6e030d0e02363d37b7f
Anderweys <anderweys999@gmail.com>
2023-08-02 19:13:00 +0300

    Add Marten for Event sorcing.

---
+++ src/CargoTransportation/Services/Cargo/Cargo.API/Extensions/AddMartenExtension.cs
@@ -0,0 +1,17 @@
+﻿using CargoObject.Domain.AggregatesModel.CargoAggregates;
+using Microsoft.EntityFrameworkCore;
+using Marten;
+
+namespace CargoObject.API.Extensions;
+
+public static class AddMartenExtension
+{
+    public static void AddMarten(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMarten(opt =>
+        {
+            opt.Connection(configuration.GetConnectionString("EventStore"));
+            opt.Projections.Snapshot<Cargo>(Marten.Events.Projections.SnapshotLifecycle.Inline);
+        });
+    }
+}
