e7c34bedc40c0587c2f45507fa38b98ba4b8c0f8
Anderweys <anderweys999@gmail.com>
2023-08-02 19:16:37 +0300

    Update Item ID.

--- src/CargoTransportation/Services/Cargo/Cargo.Application/Models/Item.cs
+++ src/CargoTransportation/Services/Cargo/Cargo.Application/Models/Item.cs
@@ -1,5 +1,5 @@
 ﻿namespace CargoObject.Application.Models;
 
 public record Item(
-    int Id, string Name, string Description,
+    Guid Id, string Name, string Description,
     float Length, float Width, float Height, float Price);
